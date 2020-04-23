8f5a3a416b0b9d4dd8fa05debff8fd913cb0fe7f
Tim Lai <tlai4@disroot.org>
2020-04-23 18:41:16 -0400

    Cross reference with new ChIP

--- genes_crosslist.py
+++ genes_crosslist.py
@@ -8,12 +8,12 @@ Created on Sat Apr 18 22:53:25 2020
 import pandas as pd
 
 df = pd.read_csv("ensembl_ids_names.tsv", sep = ' ', index_col = False)
-genes = set(df['external_gene_name'])
-with open("FlagERG_uniqueGenes.txt", 'r') as f:
+genes = set(df['ensembl_gene_id'])
+with open("chip/flagERG_uniqueGenes.txt", 'r') as f:
     ERG = set(f.read().splitlines())
 
 targets = ERG.intersection(genes)
 
-with open('target.txt', 'w') as f:
+with open('target38.txt', 'w') as f:
     for target in targets:
         f.write("%s\n" % target)
