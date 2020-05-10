1f20a455af2c2c11250cd0139a4d26fbbe7dbbdc
Tim Lai <tlai4@disroot.org>
2020-05-10 15:29:12 -0400

    Extract 5'utr sequences

--- RWPE1/ids2names.R
+++ RWPE1/ids2names.R
@@ -3,5 +3,5 @@ df <- read.csv("condition_ERG_results.csv", stringsAsFactors = FALSE)
 ids <- df[,1]
 mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
 genes <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"),values=ids, mart=mart)
-write.table(genes, "ensembl_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE)
+write.table(genes, "ensembl_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE, sep = "\t")
 
---
+++ RWPE1/target2sequences.R
@@ -0,0 +1,5 @@
+library(biomaRt)
+geneIds <- read.csv("target38.txt", stringsAsFactors = FALSE, header = FALSE)
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+sequences <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "5utr"),values=geneIds, mart=mart)
+write.table(sequences, "target38_sequences.csv", col.names=FALSE, row.names=FALSE, quote=FALSE, sep = "\t")
