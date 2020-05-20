24f466293447296e466034da72e4e91abbe98ba1
Tim Lai <tlai4@disroot.org>
2020-05-20 19:07:56 -0400

    Initial commit

--- ids2bed.R
+++ ids2bed.R
@@ -7,6 +7,15 @@ bed <- getBM(filters= "ensembl_gene_id",
              attributes=c("chromosome_name", "start_position", "end_position", "ensembl_gene_id", "strand"),
              values=ids, mart=mart)
 bed <- add_column(bed, score = 0, .before = "strand")
-str_replace(str_replace(bed$strand, "-1", "-"), "1", "+")
+bed$strand <- str_replace(str_replace(bed$strand, "-1", "-"), "1", "+")
 write.table(bed, "s2p/fimo.bed", col.names=FALSE, row.names=FALSE, quote=FALSE, sep = "\t")
 
+# Check non motifs
+targets <- read.table("RWPE1/target38.txt", stringsAsFactors = FALSE)
+ids <- setdiff(targets$V1, bed$ensembl_gene_id)
+bed <- getBM(filters= "ensembl_gene_id", 
+             attributes=c("chromosome_name", "start_position", "end_position", "ensembl_gene_id", "strand"),
+             values=ids, mart=mart)
+bed <- add_column(bed, score = 0, .before = "strand")
+bed$strand <- str_replace(str_replace(bed$strand, "-1", "-"), "1", "+")
+write.table(bed, "s2p/nonfimo.bed", col.names=FALSE, row.names=FALSE, quote=FALSE, sep = "\t")
