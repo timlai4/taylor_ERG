fd84007a60630ea3dc05d617c6f1d2e68bd96b9e
Tim Lai <tlai4@disroot.org>
2020-06-03 23:01:34 -0400

    Cluster into promoter/enhancer

--- vcap/chip/annoPeaks.R
+++ vcap/chip/annoPeaks.R
@@ -18,4 +18,13 @@ plotDistToTSS(annotated)
 annotated  <- as.data.frame(annotated)
 write.table(annotated, "annotated_peaks.tsv", sep = '\t', col.names = TRUE, 
             row.names = FALSE, quote = FALSE, na = "")
-            
+
+# We are interested in considering promoters vs. enhancers
+# Here, we define promoter to be +/- 500 from TSS.
+promoters <- annotated$annotation == "Promoter (<=1kb)" 
+# Now, save as BED files for downstream analyses.
+bed_columns <- c(1, 2, 3, 6, 7)
+write.table(annotated[promoters, bed_columns], "promoter_peaks.bed", sep = '\t', 
+            col.names = FALSE, row.names = FALSE, quote = FALSE, na = "")
+write.table(annotated[!promoters, bed_columns], "enhancer_peaks.bed", sep = '\t', 
+            col.names = FALSE, row.names = FALSE, quote = FALSE, na = "")
