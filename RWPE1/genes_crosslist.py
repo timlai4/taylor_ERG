6237d511178e56a7eb2aba1c34613bbcb7666fcb
Tim Lai <tlai4@disroot.org>
2020-06-15 14:49:24 -0400

    Added gene names

--- RWPE1/genes_crosslist.py
+++ RWPE1/genes_crosslist.py
@@ -7,8 +7,8 @@ Created on Sat Apr 18 22:53:25 2020
 
 import pandas as pd
 
-df = pd.read_csv("ensembl_ids_names.tsv", sep = '\t', index_col = False)
-genes = set(df['ensembl_gene_id'])
+df = pd.read_csv("rna/condition_ERG_results.csv", index_col = False)
+genes = set(df.iloc[:,0])
 with open("chip/flagERG_uniqueGenes.txt", 'r') as f:
     ERG = set(f.read().splitlines())
 
@@ -18,17 +18,16 @@ with open('target38.txt', 'w') as f:
     for target in targets:
         f.write("%s\n" % target)
 
-
-df = pd.read_csv("ERG_activated_ids_names.tsv", sep = '\t', index_col = False)
-genes = set(df['ensembl_gene_id'])
+df = pd.read_csv("rna/ERG_activated.csv", index_col = False)
+genes = set(df.iloc[:,0])
 targets = ERG.intersection(genes)
 
 with open('activated_target38.txt', 'w') as f:
     for target in targets:
         f.write("%s\n" % target)
         
-df = pd.read_csv("ERG_repressed_ids_names.tsv", sep = '\t', index_col = False)
-genes = set(df['ensembl_gene_id'])
+df = pd.read_csv("rna/ERG_repressed.csv", index_col = False)
+genes = set(df.iloc[:,0])
 targets = ERG.intersection(genes)
 
 with open('repressed_target38.txt', 'w') as f:
--- RWPE1/ids2names.R
+++ RWPE1/ids2names.R
@@ -1,8 +1,16 @@
 library(biomaRt)
-df <- read.csv("rna/condition_ERG_results.csv", stringsAsFactors = FALSE)
+df <- read.table("target38.txt", stringsAsFactors = FALSE)
 ids <- df[,1]
 mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-genes <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"),values=ids, mart=mart)
-write.table(genes, "ensembl_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE, sep = "\t")
+genes <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"), values=ids, mart=mart)
+write.table(genes, "target38_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE, sep = "\t")
 
+df <- read.table("activated_target38.txt", stringsAsFactors = FALSE)
+ids <- df[,1]
+genes <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"), values=ids, mart=mart)
+write.table(genes, "activated_target38_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE, sep = "\t")
 
+df <- read.table("repressed_target38.txt", stringsAsFactors = FALSE)
+ids <- df[,1]
+genes <- getBM(filters= "ensembl_gene_id", attributes=c("ensembl_gene_id", "external_gene_name"), values=ids, mart=mart)
+write.table(genes, "repressed_target38_ids_names.tsv", col.names=TRUE, row.names=FALSE, quote=FALSE, sep = "\t")
