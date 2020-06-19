f8b03b11f8e0bb645dd9a1b5153105de077946da
Tim Lai <tlai4@disroot.org>
2020-06-18 20:56:31 -0400

    Added enhancers

--- vcap/chip/annoPeaks.R
+++ vcap/chip/annoPeaks.R
@@ -24,8 +24,12 @@ write.table(annotated, "annotated_peaks.tsv", sep = '\t', col.names = TRUE,
 promoters <- annotated$annotation == "Promoter (<=1kb)" 
 gene_bed_columns <- c(13, 14, 15, 18, 7, 17) # The columns in annotated that comprise the BED information.
 promoter_genes <- annotated[promoters, gene_bed_columns]
+enhancer_genes <- annotated[!promoters, gene_bed_columns]
 promoter_genes <- mutate(promoter_genes, geneStrand = ifelse(geneStrand == 1, "+", "-"))
+enhancer_genes <- mutate(enhancer_genes, geneStrand = ifelse(geneStrand == 1, "+", "-"))
 write.table(promoter_genes, "promoter_genes.bed", sep = '\t', 
+            col.names = FALSE, row.names = FALSE, quote = FALSE, na = "")
+write.table(enhancer_genes, "enhancer_genes.bed", sep = '\t', 
             col.names = FALSE, row.names = FALSE, quote = FALSE, na = "")
 # Now, save as BED files for downstream analyses.
 bed_columns <- c(1, 2, 3, 6, 7)
